c845850c1fcab7c4ea9454d1464472e9d113725c
Romain Beauxis <toots@rastageeks.org>
2012-07-21 16:49:31 -0500

    Simplier #ifdef for inclusing ARM asm.

--- src/lib/g_includes.h
+++ src/lib/g_includes.h
@@ -1,11 +1,7 @@
 #include "types.h"
 
-#ifdef __INLINE_ASM
-
-    #if defined(__ARM_ARCH_5TE__) || defined( __XSCALE__) || defined(__ARM_ARCH_5T__) || defined(__ARM_ARCH_4__) || defined(__STRONGARM__)
-    #include "mult_sarm_gcc.h"
-    #else
-    #include "mult_noarch_gcc.h"
-    #endif
-
+#if defined(__arm__)
+#include "mult_sarm_gcc.h"
+#else
+#include "mult_noarch_gcc.h"
 #endif
