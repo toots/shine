b4323cbcf257997674549a017b3ebcb2ad7587ab
Romain Beauxis <toots@rastageeks.org>
2012-07-25 21:18:18 -0500

    More clever multiplication..

--- src/lib/mult_noarch_gcc.h
+++ src/lib/mult_noarch_gcc.h
@@ -2,11 +2,11 @@
 
 #include <stdint.h>
 
-#define mul(a,b)   (int32_t)  ( (     ((int64_t) a) * ((int64_t) b) ) >>32 )
+#define mul(a,b)   (int32_t)  ( ( ((int64_t) a) * ((int64_t) b) ) >>32 )
 
-#define muls(a,b)  (int32_t)  ( ( 2 * ((int64_t) a) * ((int64_t) b) ) >>32 )
+#define muls(a,b)  (int32_t)  ( ( ((int64_t) a) * ((int64_t) b) ) >>31 )
 
-#define mulr(a,b)  (int32_t)  ( ( (    ((int64_t) a) * ((int64_t) b)) + 0x80000000 ) >>32 )
+#define mulr(a,b)  (int32_t)  ( ( ( ((int64_t) a) * ((int64_t) b)) + 0x80000000 ) >>32 )
 
-#define mulsr(a,b) (int32_t)  ( ( ( 2* ((int64_t) a) * ((int64_t) b)) + 0x80000000 ) >>32 )
+#define mulsr(a,b) (int32_t)  ( ( ( ((int64_t) a) * ((int64_t) b)) + 0x80000000 ) >>31 )
 
