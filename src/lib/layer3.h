36e96d4a6725437a0e83eac558005e6b6c827673
Romain Beauxis <toots@rastageeks.org>
2012-07-28 15:44:00 -0500

    Documented layer3.h

--- src/lib/layer3.h
+++ src/lib/layer3.h
@@ -21,7 +21,7 @@ typedef struct {
     long          samplerate;
 } shine_wave_t;
 
-/* This is the struct the encoder uses to hold informationn about the output MP3 */
+/* This is the struct the encoder uses to tell the encoder about the output MP3 */
 
 enum modes {
   STEREO       = 0,
@@ -38,7 +38,7 @@ enum emph {
 
 typedef struct {
     enum modes mode;      /* Stereo mode */
-    int        bitr;      /* Must conform to known bitrate - see Main.c */
+    int        bitr;      /* Must conform to known bitrate */
     enum emph  emph;      /* De-emphasis */
     int        copyright;
     int        original;
@@ -49,16 +49,47 @@ typedef struct {
   shine_mpeg_t mpeg;
 } shine_config_t;
 
+/* Abtract type for the shine encoder handle. */
 typedef struct shine_global_flags *shine_t;
 
+/* Fill in a `mpeg_t` structure with default values. */
 void L3_set_config_mpeg_defaults(shine_mpeg_t *mpeg);
+
+/* Check if a given bitrate is supported by the encoder (see `bitrates` above for a list
+ * of acceptable values. */
 int L3_find_bitrate_index(int bitr);
+
+/* Check if a given bitrate is supported by the encoder (see `samplerates` above for a list
+ * of acceptable values. */
 int L3_find_samplerate_index(long freq);
 
+/* Pass a pointer to a `config_t` structure and returns an initialized
+ * encoder. 
+ *
+ * Configuration data is copied over to the encoder. It is not possible
+ * to change its values after initializing the encoder at the moment.
+ *
+ * Checking for valid configuration values is left for the application to 
+ * implement. You can use the `L3_find_bitrate_index` and 
+ * `L3_find_samplerate_index` functions above, or the `bitrates` and 
+ * `samplerates` arrays above to check those parameters. Mone and stereo 
+ * mode for wave and mpeg should als be consistent with each other.
+ *
+ * This function returns NULL if it was not able to allocate memory data for 
+ * the encoder. */
 shine_t L3_initialise(shine_config_t *config);
 
+/* Encode audio data. Source data must have `samp_per_frames` audio samples per
+ * channels. Mono encoder only expect one channel. 
+ *
+ * Returns a pointer to newly encoded data while `written` contains the size of
+ * available encoded data. This pointer's memory is handled by the library and 
+ * is only valid until the next call to `L3_encode_frame` or `L3_close` and may
+ * be NULL if no data was written. */
 unsigned char *L3_encode_frame(shine_t s, int16_t data[2][samp_per_frame], long *written);
 
+/* Close an encoder, freeing all associated memory. Encoder handler is not
+ * valid after this call. */
 void L3_close(shine_t s);
 
 #endif
