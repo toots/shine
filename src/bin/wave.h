d7a91d3eeb988d6ca081029aa298cd208a83489c
Matthias Grosser <mtgrosser@gmx.net>
2013-01-20 17:36:13 +0100

    Rewrite WAVE functions to respect unknown chunks of different size

--- src/bin/wave.h
+++ src/bin/wave.h
@@ -2,9 +2,15 @@
 #define WAVE_H
 
 #include <stdio.h>
+#include <stdbool.h>
 
-FILE *wave_open(const char *file, shine_config_t *config, int quiet);
-int  wave_get(int16_t buffer[2][samp_per_frame], FILE *file, void *config_in);
-void wave_close(FILE *file);
+typedef struct {
+        FILE *file;
+        long length;
+} wave_t;
+
+bool wave_open(const char *fname, wave_t *wave, shine_config_t *config, int quiet, long *data_length);
+int  wave_get(int16_t buffer[2][samp_per_frame], wave_t *wave, void *config_in);
+void wave_close(wave_t *wave);
 
 #endif
