098b9aaa6770a41bdf3f9c049307c1398f04d2d3
Romain Beauxis <toots@rastageeks.org>
2022-12-30 12:23:02 +0100

    Fix this.

--- src/lib/l3mdct.h
+++ src/lib/l3mdct.h
@@ -3,7 +3,7 @@
 
 #include "types.h"
 
-void shine_mdct_initialise();
+void shine_mdct_initialise(shine_global_config *config);
 void shine_mdct_sub(shine_global_config *config, int stride);
 
 #endif
