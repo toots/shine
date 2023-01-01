2d981a0ba0cf52c69f58a71dbe1822845da9d096
Romain Beauxis <toots@rastageeks.org>
2023-01-01 11:53:09 +0100

    Fix duplicated code.

--- js/src/libshine_node_wrapper.js
+++ js/src/libshine_node_wrapper.js
@@ -7,14 +7,4 @@ Shine.initialized = new Promise(function (resolve) {
 })
 
 Module["Shine"] = Shine;
-
-Module["StereoMode"] = {
-  '0': 'STEREO',
-  '1': 'JOINT_STEREO',
-  '2': 'DUAL_CHANNEL',
-  '3': 'MONO',
-  STEREO: 0,
-  JOINT_STEREO: 1,
-  DUAL_CHANNEL: 2,
-  MONO: 3
-};
+Module["StereoMode"] = StereoMode;
